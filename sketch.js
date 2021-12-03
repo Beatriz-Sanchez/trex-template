f9085cfe204f9f217fa003c306a2c92e7c93a2f4
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-12-03 10:16:32 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var pontos = 0;
 var JOGAR = 1;
 var ENCERRAR = 0;
 var estadoJogo = JOGAR;
+var gameOverImg, reiniciarImg, gameOver, reiniciar;
 
 function preload() {
   trex_correndo = loadAnimation("trex1.png", "trex3.png", "trex4.png");
@@ -21,6 +22,10 @@ function preload() {
   cacto4 = loadImage("obstaculo4.png");
   cacto5 = loadImage("obstaculo5.png");
   cacto6 = loadImage("obstaculo6.png");
+
+  gameOverImg = loadImage("fimDoJogo.png");
+  reiniciarImg = loadImage("reiniciar.png");
+
 }
 
 function setup() {
@@ -39,25 +44,34 @@ function setup() {
 
   soloInvisivel = createSprite(300, 180, 1200, 5);
   soloInvisivel.visible = false;
-  
+
   grupoNuvens = new Group();
   grupoCactos = new Group();
 
+  gameOver = createSprite(300, 80);
+  gameOver.addImage(gameOverImg);
+  gameOver.scale = 0.5;
+  gameOver.visible = false;
+
+  reiniciar = createSprite(300, 120);
+  reiniciar.addImage(reiniciarImg);
+  reiniciar.scale = 0.5;
+  reiniciar.visible = false;
 }
 
 function draw() {
   background("white");
-  text("Pontos: "+pontos,500,20);
+  text("Pontos: " + pontos, 500, 20);
 
   trex.collide(soloInvisivel);
 
   drawSprites();
-  
+
   //comportamentos que só ocorrem no estado de jogo JOGAR
-  if (estadoJogo === JOGAR){
-    
+  if (estadoJogo === JOGAR) {
+
     //aumentar pontos
-    pontos = Math.round((pontos+(frameCount/3))/2);
+    pontos = Math.round((pontos + (frameCount / 3)) / 2);
 
     //pulo
     if (keyDown("space") && trex.y > 153) {
@@ -65,8 +79,8 @@ function draw() {
     }
 
     //gravidade
-    trex.velocityY = trex.velocityY + 1.5 ;
-    
+    trex.velocityY = trex.velocityY + 1.5;
+
     //criando o solo infinito
     solo.velocityX = -6;
     if (solo.x < 0) {
@@ -76,21 +90,35 @@ function draw() {
     //gerando nuvens e cactos
     gerarNuvens();
     gerarCactos();
-    
+
     //momento em que o jogo acaba
-    if (grupoCactos.isTouching(trex)){
-      estadoJogo = ENCERRAR
+    if (grupoCactos.isTouching(trex)) {
+      estadoJogo = ENCERRAR;
     }
-  
-  //comportamentos que só ocorrem no estado de jogo ENCERRAR
-  } else if (estadoJogo === ENCERRAR){
-    
+
+    //comportamentos que só ocorrem no estado de jogo ENCERRAR
+  } else if (estadoJogo === ENCERRAR) {
+
     //parar o solo
     solo.velocityX = 0;
-    
+
     //parar as nuvens e obstáculos
     grupoCactos.setVelocityXEach(0);
     grupoNuvens.setVelocityXEach(0);
+
+    //torna as nuvens e os cactos infinitos
+    grupoCactos.setLifetimeEach(-1);
+    grupoNuvens.setLifetimeEach(-1);
+
+    //muda a animação do trex
+    trex.changeAnimation("colidiu", trex_colidiu);
+
+    //impede que o trex voe ao encerrar o jogo
+    trex.velocityY = 0;
+
+    //tornar imagens de fim de jogo visíveis
+    gameOver.visible = true;
+    reiniciar.visible = true;
   }
 }
 
@@ -110,28 +138,35 @@ function gerarNuvens() {
   }
 }
 
-function gerarCactos(){
+function gerarCactos() {
   //criar sprite de obstáculo a cada 60 quadros
-  if(frameCount %60 === 0){
-    cacto = createSprite(600,155,10,40);
-    cacto.velocityX= -6;
-  
-  //adicionar imagem ao obstaculo aleatoriamente
-    var rand = Math.round(random(1,6));
-    switch(rand){
-      case 1: cacto.addImage(cacto1);
-        	break;
-      case 2: cacto.addImage(cacto2);
-        	break;
-   	  case 3: cacto.addImage(cacto3);
-        	break;
-      case 4: cacto.addImage(cacto4);
-        	break;
-      case 5: cacto.addImage(cacto5);
-        	break;
-      case 6: cacto.addImage(cacto6);
-        	break;
-      default: break;
+  if (frameCount % 60 === 0) {
+    cacto = createSprite(600, 155, 10, 40);
+    cacto.velocityX = -6;
+
+    //adicionar imagem ao obstaculo aleatoriamente
+    var rand = Math.round(random(1, 6));
+    switch (rand) {
+      case 1:
+        cacto.addImage(cacto1);
+        break;
+      case 2:
+        cacto.addImage(cacto2);
+        break;
+      case 3:
+        cacto.addImage(cacto3);
+        break;
+      case 4:
+        cacto.addImage(cacto4);
+        break;
+      case 5:
+        cacto.addImage(cacto5);
+        break;
+      case 6:
+        cacto.addImage(cacto6);
+        break;
+      default:
+        break;
     }
     //atribuir escala e tempo de vida aos obstáculos
     cacto.scale = 0.5;
